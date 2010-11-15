2f827af1b02a2b7ae8ecdf39bc0fd37a51b0ae0b
James Healy <jimmy@deefa.com>
2010-11-14 23:05:18 -0600

    make loading of strategy classes dynamic
    * no need to register each strategy, just place them in the
      standard strategies directory

--- lib/gem_lint.rb
+++ lib/gem_lint.rb
@@ -6,29 +6,20 @@ require 'fileutils'
 
 # our own code
 require 'gem_lint/runner'
-require 'gem_lint/strategies/abstract_strategy'
-require 'gem_lint/strategies/bin_ends_with_rb_strategy'
-require 'gem_lint/strategies/bin_without_shebang_strategy'
-require 'gem_lint/strategies/changelog_strategy'
-require 'gem_lint/strategies/csv_email_strategy'
-require 'gem_lint/strategies/no_pkg_dir_strategy'
-require 'gem_lint/strategies/readme_strategy'
-require 'gem_lint/strategies/require_matches_gemname_strategy'
-require 'gem_lint/strategies/ruby_file_location_strategy'
-require 'gem_lint/strategies/utf8_metadata_strategy'
+
+# dynamically load all available strategies
+strategies_path = File.dirname(__FILE__) + "/gem_lint/strategies/**/*.rb"
+Dir[strategies_path].sort.each {|f| require f }
 
 module GemLint
+
+  # return an array of all strategy classes
+  #
   def self.strategies
-    [
-      GemLint::Strategies::BinEndsWithRbStrategy,
-      GemLint::Strategies::BinWithoutShebangStrategy,
-      GemLint::Strategies::ChangelogStrategy,
-      GemLint::Strategies::CsvEmailStrategy,
-      GemLint::Strategies::NoPkgDirStrategy,
-      GemLint::Strategies::ReadmeStrategy,
-      GemLint::Strategies::RequireMatchesGemnameStrategy,
-      GemLint::Strategies::RubyFileLocationStrategy,
-      GemLint::Strategies::Utf8MetadataStrategy
-    ]
+    GemLint::Strategies.constants.sort.select { |class_name|
+      class_name != "AbstractStrategy"
+    }.map { |class_name|
+      GemLint::Strategies.const_get(class_name)
+    }
   end
 end
