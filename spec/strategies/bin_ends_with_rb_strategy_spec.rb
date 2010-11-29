dfaba689a00392b88cad223c415d4b62c934798b
James Healy <jimmy@deefa.com>
2010-11-28 21:38:09 -0800

    update some copied specs

--- spec/strategies/bin_ends_with_rb_strategy_spec.rb
+++ spec/strategies/bin_ends_with_rb_strategy_spec.rb
@@ -4,12 +4,12 @@ require "gem_lint"
 
 describe GemLint::Strategies::BinEndsWithRbStrategy do
 
-  it "correctly fail gems with a pkg dir" do
+  it "correctly fail gems with an executable ending in .rb" do
     args = strategy_args("bin_has_rb")
     GemLint::Strategies::BinEndsWithRbStrategy.new(args).fail?.should be_true
   end
 
-  it "correctly pass gems with no pkg dir" do
+  it "correctly pass gems with no executable ending in .rb" do
     args = strategy_args("bin_has_no_rb")
     GemLint::Strategies::BinEndsWithRbStrategy.new(args).fail?.should be_false
   end
