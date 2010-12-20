b5dcfb84c0aced2c1386a0ba646158186cb99023
James Healy <jimmy@deefa.com>
2010-12-20 23:59:23 +1100

    load path ugliness to get specs running on 1.9

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require 'rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir["spec/support/**/*.rb"].each {|f| require f}
+Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f }
 
 RSpec.configure do |config|
   config.include GemLintSpecHelper
